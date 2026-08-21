60522f9c12fd19742059f7264bad9840118b1504
RAHAT-UL-FARDOUSH RAZ <205417500+raz-rahat@users.noreply.github.com>
2026-08-21 22:46:40 +0600

    Update Single_LED_Blink.ino

--- Single_LED_Blink.ino
+++ Single_LED_Blink.ino
@@ -1,26 +1,13 @@
-int ledPin1 = 13; 
-int ledPin2 = 12;
-int ledPin3 = 11;
+int ledPin = 13; 
 
 void setup() {
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin1, HIGH);
-  digitalWrite(ledPin2, LOW);
-  digitalWrite(ledPin3, LOW);
-  delay(1000);
+  digitalWrite(ledPin, HIGH);  
+  delay(1000);                 
 
-  digitalWrite(ledPin1, LOW);
-  digitalWrite(ledPin2, HIGH);
-  digitalWrite(ledPin3, LOW);
-  delay(1000);
-
-  digitalWrite(ledPin1, LOW);
-  digitalWrite(ledPin2, LOW);
-  digitalWrite(ledPin3, HIGH);
-  delay(1000);
+  digitalWrite(ledPin, LOW);   
+  delay(1000);                 
 }
